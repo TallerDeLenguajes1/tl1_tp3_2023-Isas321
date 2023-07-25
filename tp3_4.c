80454795199968642b94fd917d7c16b91a82483e
Isas321 <javier.isas321@gmail.com>
2023-07-24 21:16:51 -0300

    4 mejor

--- tp3_4.c
+++ tp3_4.c
@@ -3,23 +3,29 @@
 #include <string.h>
 
 struct producto{
-    int productoID; //Numerado en ciclo iterativo
-    int cantidad; // entre 1 y 10
-    char *TipoProducto; // Algún valor del arreglo TiposProductos
-    float precioUnitario;// entre 10 - 100 
+    int productoID;         //Numerado en ciclo iterativo
+    int cantidad;       // entre 1 y 10
+    char *TipoProducto;         // Algún valor del arreglo TiposProductos
+    float precioUnitario;       // entre 10 - 100 
 };
 
 struct cliente{
-    int clienteID; 
-    char nombreCliente;
-    int cantProductosAPedir;
-    struct producto *productos;
+    int clienteID;      // Numerado en el ciclo iterativo
+    char nombreCliente;     // Ingresado por usuario
+    int cantProductosAPedir;        // (aleatorio entre 1 y 5)
+    struct producto *productos;     //El tamaño de este arreglo depende de la variable
 };
 
 
 
 int main(){
     char *TipoProductos[]={"Galletas", "Snak", "Cigarros", "Caramelos", "Bebidas"};
+    int cantClientes;
+    
+    printf("\nCantidad de clientes: ");
+    scanf("%d", cantClientes);
+    getchar();
+
 
     return 0;
 }
